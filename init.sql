83984dfd58045814fb6dc3a249ed2a63bb2e2e9c
David Tisalema <letisidw@gmail.com>
2025-06-23 20:52:47 -0500

    username add

--- init.sql
+++ init.sql
@@ -2,6 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
     user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(255) UNIQUE NOT NULL, -- << AÑADIDO
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     role VARCHAR(50) DEFAULT 'buyer', -- seller, admin, etc.
